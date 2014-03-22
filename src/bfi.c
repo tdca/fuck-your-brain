92a452405b142e51198d524deb1193b89995e311
Kimmy Leo <kenpusney@gmail.com>
2014-03-22 00:41:35 -0400

    FIX BUG! ======= features:    $ .. ; for procedure definition    *... ! for procedure call   see `example/funcall.bf`

--- src/bfi.c
+++ src/bfi.c
@@ -1,61 +1,98 @@
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "stack.h"
 
 #define T 65536
 
 int memory[T];
 char instruction[T];
+
+unsigned char iset[256] = {0};
+#define regist_ins(c) iset[(int)c] = 1
+
 stack_t block;
-//stack_t fun_stack;
+stack_t fun;
+stack_t call;
 //stack_t data_stack;
 //stack_t addr_stack;
+int p=0,f=0,i=0;
+
+void debug();
+
+void init_iset(){
+	// basic
+	regist_ins('>');
+	regist_ins('<');
+	regist_ins('+');
+	regist_ins('-');
+	regist_ins('.');
+	regist_ins(',');
+	regist_ins('[');
+	regist_ins(']');
+	
+	// ext-fun
+	regist_ins('$');
+	regist_ins('^');
+	regist_ins(';');
+	regist_ins('*');
+	regist_ins('!');
+}
 
 void exec(){
-	int p=0,i=0,f=1;	//pointer,iterator,flow-flag,point-register
-	char c='\1';	//current-instruction,
+	int fdef = 0,fcall = 0;
+	char c='\1';	//current-instruction
 	while(c){
-		c=instruction[i];	//get each instruction
-		if(!f&&c!=']'){	//if flow-control was not avalible
-			i++;
-			continue;
-		}else{
-			switch(c){
-				case '>': p=(p+1)%T;break;	//pointer moves right
-				case '<': p=(p-1)%T;break;	//pointer moves left
-				case '+': memory[p]++;break;	//increase the byte at the pointer
-				case '-': memory[p]--;break;	//decrease the byte at the pointer
-				case '.': putchar(memory[p]);break;	//out put the byte
-				case ',': memory[p]=getchar();break;	//get an value & put it into byte at the pointer
-				case '[': (memory[p]?(f=1,push(&block,i)):(f=0));break;	//flow-control begins
-				case ']': (memory[p]?(i=top(&block)):pop(&block));break;	//flow-control ends
-				default:break;	//else then do nothing
-			}
+		c=instruction[i++];	//get each instruction
+		if(f&&(c!=']')){ continue; }
+		if(fdef&&(c!=';')){ continue; }
+		switch(c){
+			case '>': p=(p+1)%T;  break;
+			case '<': p=(p-1)%T;  break;
+			case '+': memory[p]++;  break;
+			case '-': memory[p]--;  break;
+			case '.': putchar(memory[p]);  break;
+			case ',': if((memory[p]=fgetc(stdin)) == EOF) return; break;
+			case '[': if(memory[p]){ f = 0; push(&block,i);}else{ f = 1; }       break;
+			case ']': (memory[p]?(i=top(&block)):(pop(&block),f=0));    break;
+			case '$': push(&fun,i); fdef=1; break;
+			case ';': if(fdef) fdef = 0;else i=pop(&call); break;
+			case '^': i=pop(&call);  break;
+			case '*': fcall++;       break;
+			case '!': push(&call,i); i = fun.chunk[fcall]; fcall=0; break;
+			default:break;
 		}
-		i++;	//next instruction
 	}
 }
 
 int main(int argc,char* argv[]){
 	FILE* b;
-	char c;
+	short c;
 	int i=0;
+	init_iset();
 	if(argc-1){
-		b=fopen(argv[1],"r");
-		if( !b ){
-			perror("Error: Could not open program file");
-			exit(-1);
-		}
+		if( !(b=fopen(argv[1],"r")) ) return -1;
 		while((c=fgetc(b))!=EOF)
-			if(c=='<'||c=='>'||c==','||c=='.'||c=='-'||c=='+'||c=='['||c==']')
-				instruction[i++] = c;
+			if(iset[c]) instruction[i++] = c;
         fclose(b);
+		exec();
 	}else{
-	    puts("Interactive mode now on.");
-	    puts("Brainfuck Intepreter by KPSN.Leo");
-		scanf("%s",instruction);	//get instructions source code
+	    puts("Interactive mode now on");
+	    puts("Brainfuck Intepreter");
+	    puts("  --  By Kimmy Leo <kenpusney@gmail.com>");
+	    printf("- ");
+	    while(scanf("%s",instruction+i) != EOF){
+	    	exec();
+	    	printf("\n- ");
+	    }
+	    puts("\nByebye!");
 	}
-	exec();
 	return 0;
+}
+
+void debug(){
+	int c;
+	while((c = pop(&fun))){
+		printf("%d",c);
+		printf("%c",instruction[c]);
+	}
 }
